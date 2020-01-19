adbc64a8ec75e8a7a85202ab370c69e9fe6365b1
Chance Nelson <cbn35@nau.edu>
2020-01-18 23:15:30 -0700

    set port to 80

--- run.py
+++ run.py
@@ -8,4 +8,6 @@ app = api.create_app()
 CORS(app)
 socketio = SocketIO(app)
 
-socketio.run(app, debug=True, host='0.0.0.0')
+api.room.generate_sockets(socketio)
+
+socketio.run(app, debug=True, host='0.0.0.0', port=80)
