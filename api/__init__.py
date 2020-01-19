1b82c30926c94e1c8f975f14a57a7e399b0001ae
Chance Nelson <cbn35@nau.edu>
2020-01-18 23:35:24 -0700

    attempted fix of CORS blockage of socket messages

--- api/__init__.py
+++ api/__init__.py
@@ -1,14 +1,25 @@
 from mongoengine import connect
 from flask import Flask, render_template
+from flask_socketio import SocketIO
+from flask_cors import CORS
 
-from .room import ROOM_BP
+from .room import ROOM_BP, generate_sockets
 
 
 def create_app():
+    socketio = SocketIO()
+
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'secret!'
     app.config['DB'] = connect()
 
+    CORS(app)
+
     app.register_blueprint(ROOM_BP)
 
-    return app
+    socketio.init_app(app, cors_allowed_origins='*')
+
+    generate_sockets(socketio)
+
+
+    return (app, socketio)
--- run.py
+++ run.py
@@ -4,10 +4,6 @@ from flask_cors import CORS
 import api
 
 
-app = api.create_app()
-CORS(app)
-socketio = SocketIO(app)
-
-api.room.generate_sockets(socketio)
+app, socketio = api.create_app()
 
 socketio.run(app, debug=True, host='0.0.0.0', port=80)
