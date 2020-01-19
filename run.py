27812765ca6589342e278a01e35d83fe7bb701a2
Chance Nelson <cbn35@nau.edu>
2020-01-18 17:09:03 -0700

    add cors

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 from flask_socketio import SocketIO
+from flask_cors import CORS
 
 import api
 
 
 app = api.create_app()
+CORS(app)
 socketio = SocketIO(app)
 
 socketio.run(app, debug=True, host='0.0.0.0')
